30f959214b0ab910b1f4f6284e6c8dac9bb0f627
diltheshi <diltheshi@gmail.com>
2020-09-17 16:39:11 +0530

    hello

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+</head>
+
+<body>
+    Welcome <?php echo $_POST["name"];?><br>
+    <h1>You have entered the following data</h1>
+    Gender <?php echo $_POST["gender"];?><br>
+    Index Number <?php $_POST["IndexNumber"];?><br>
+    Address <?php $_POST["address"];?><br>
+    Email <?php $_POST["email"]; ?>
+    
+
+
+
+</body>
+    
+</html>
